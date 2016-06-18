b45a74543ad6798db0fea2e4a5df6ed6015dad71
arm <adam.melnyk@gmail.com>
2016-06-18 17:37:42 -0600

    Adding functionality for calculating price after markup
    * Added tests for each category that gets it's own markup
    * Added tests for extra workers
    * Updated information in the gemspec which is used on ruby gems:
      * author, gem description, homepage
    * Added the package command to the module
      * Method that caluclates the price after all markup is applied
    * Added two helper methods for calculating the worker markup and the category
      markup
    * Updated README with useage instructions

--- spec/repack_spec.rb
+++ spec/repack_spec.rb
@@ -5,7 +5,30 @@ describe Repack do
     expect(Repack::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#self.package' do
+    it 'adds the pharmaceuticals markup' do
+      total = Repack.package(5432.00, 1, "drugs")
+      expect(total).to eq 6199.81
+    end
+
+    it 'adds the food markup' do
+      total = Repack.package(1299.99, 3, "food")
+      expect(total).to eq 1591.58
+    end
+
+    it 'adds the electronics markup' do
+      total = Repack.package(1000, 1, "electronics")
+      expect(total).to eq 1083.60
+    end
+
+    it 'adds no extra category markup' do
+      total = Repack.package(12456.95, 4, "books")
+      expect(total).to eq 13707.63
+    end
+
+    it 'adds markup for workers' do
+      total = Repack.package(155555.99, 15, "robots")
+      expect(total).to eq 192733.87
+    end
   end
 end
